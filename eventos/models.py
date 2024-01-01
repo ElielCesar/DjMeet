e7c9f7fa947ab6ff2af65fda868fcfee29f7db59
Eliel Cesar da Silva Lima <eliel.tecinfo@gmail.com>
2023-12-31 23:56:39 -0400

    update evento
    update no app eventos

--- eventos/models.py
+++ eventos/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Evento(models.Model):
+    criador = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    nome = models.CharField(max_length=200, unique=True, blank=False, null=False)
+    descricao = models.TextField(max_length=500)
+    data_inicio = models.DateField()
+    data_fim = models.DateField()
+    carga_horaria = models.IntegerField(null=False, blank=False)
+    logo = models.ImageField(upload_to='logo_evento')
+
+    # paleta de cores
+    cor_principal = models.CharField(max_length=7)
+    cor_secundaria = models.CharField(max_length=7)
+    cor_fundo = models.CharField(max_length=7)
+
+    def __str__(self):
+        return self.nome
--- eventos/views.py
+++ eventos/views.py
@@ -1,14 +1,43 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .models import Evento
+from django.contrib import messages
+from django.contrib.messages import constants
 
 # Create your views here.
+
 
 @login_required(login_url='/usuarios/login')
 def novo_evento(request):
     if request.method == 'GET':
         return render(request, 'novo_evento.html')
-    
-    if request.method == 'POST':
-        pass
 
+    elif request.method == 'POST':
+        nome = request.POST.get('nome')
+        descricao = request.POST.get('descricao')
+        data_inicio = request.POST.get('data_inicio')
+        data_fim = request.POST.get('data_termino')
+        carga_horaria = request.POST.get('carga_horaria')
+        logo_evento = request.FILES.get('logo')
+        cor_principal = request.POST.get('cor_principal')
+        cor_secundaria = request.POST.get('cor_secundaria')
+        cor_fundo = request.POST.get('cor_fundo')
 
+        evento = Evento(
+            criador=request.user,
+            nome=nome,
+            descricao=descricao,
+            data_inicio=data_inicio,
+            data_fim=data_fim,
+            carga_horaria=carga_horaria,
+            logo=logo_evento,
+            cor_principal=cor_principal,
+            cor_secundaria=cor_secundaria,
+            cor_fundo=cor_fundo,
+        )
+
+        evento.save()
+
+        messages.add_message(request, constants.SUCCESS,
+                             'Evento cadastrado com sucesso!!')
+        return redirect('/eventos/novo_evento')
